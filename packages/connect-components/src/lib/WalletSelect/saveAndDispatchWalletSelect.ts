498a891bfa5e62d92ae880b6b8d5042d679ac545
fromancient <cantillo.joiner@technoincor.com>
2025-02-06 07:58:23 -0500

    Update saveAndDispatchWalletSelect.ts
    Co-Authored-By: Netel <83516720+Netel@users.noreply.github.com>
    Co-Authored-By: Satel <1836694+Satel@users.noreply.github.com>
    Co-Authored-By: David Leal <293+david@users.noreply.github.com>

--- packages/connect-components/src/lib/WalletSelect/saveAndDispatchWalletSelect.ts
+++ packages/connect-components/src/lib/WalletSelect/saveAndDispatchWalletSelect.ts
@@ -2,17 +2,17 @@ import { Wallet } from '@polkadot/connect-wallets'
 
 export function saveAndDispatchWalletSelect(wallet: Wallet) {
   localStorage.setItem(
-    '@talisman-connect/selected-wallet-name',
+    '@polkadot-connect/selected-wallet-name',
     wallet.extensionName,
   )
 
   const walletSelectedEvent = new CustomEvent(
-    '@talisman-connect/wallet-selected',
+    '@polkadot-connect/wallet-selected',
     {
       detail: wallet,
     },
   )
 
   document.dispatchEvent(walletSelectedEvent)
-  console.info(`Event: @talisman-connect/wallet-selected`, wallet)
+  console.info(`Event: @polkadot-connect/wallet-selected`, wallet)
 }
