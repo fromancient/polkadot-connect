961d48274a92dc4b87281587ad7373a07f62ee28
fromancient <cantillo.joiner@technoincor.com>
2025-02-11 21:17:04 -0500

    update install extension part with comments
    Co-Authored-By: Netel <83516720+Netel@users.noreply.github.com>
    Co-Authored-By: Satel <1836694+Satel@users.noreply.github.com>
    Co-Authored-By: netel-pleb <12684117+Netel-pleb@users.noreply.github.com>
    Co-Authored-By: David <86869373+huscky@users.noreply.github.com>
    Co-Authored-By: David Leal <293+david@users.noreply.github.com>

--- packages/connect-components/src/lib/WalletSelect/InstallExtension.tsx
+++ packages/connect-components/src/lib/WalletSelect/InstallExtension.tsx
@@ -2,12 +2,16 @@ import ChevronRightIcon from '../../assets/icons/chevron-right.svg?react'
 import { WithWalletProps } from './types'
 import styles from './WalletSelect.module.css'
 
+// The InstallExtension component is responsible for rendering the UI
+// when the user needs to install a wallet extension
 export function InstallExtension({ wallet: selectedWallet }: WithWalletProps) {
+  // If the selectedWallet has a noExtensionMessage, display it
   return (
     <>
       <div className={styles['no-extension-message']}>
         {selectedWallet?.noExtensionMessage}
       </div>
+      {/* Render a button that links to the wallet's installation URL */}
       <a
         className={styles['row-button']}
         href={selectedWallet?.installUrl}
@@ -16,14 +20,17 @@ export function InstallExtension({ wallet: selectedWallet }: WithWalletProps) {
       >
         <button className={styles['row-button']}>
           <span className={styles['flex']}>
+            {/* Display the wallet's logo */}
             <img
               src={selectedWallet?.logo.src}
               alt={selectedWallet?.logo.alt}
               width={32}
               height={32}
             />
+            {/* Display the wallet's title */}
             Install {selectedWallet?.title}
           </span>
+          {/* Display a chevron-right icon */}
           <ChevronRightIcon />
         </button>
       </a>
