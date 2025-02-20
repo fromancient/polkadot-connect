0518af3c00a3093f923064a8c3feaee7ce29313c
fromancient <cantillo.joiner@technoincor.com>
2025-02-19 19:26:31 -0500

    Update index.ts
    Co-Authored-By: netel-here <196799721+netel-here@users.noreply.github.com>

--- packages/connect-wallets/src/lib/manta-wallet/index.ts
+++ packages/connect-wallets/src/lib/manta-wallet/index.ts
@@ -1,15 +1,16 @@
-import { BaseDotsamaWallet } from '../base-dotsama-wallet'
-import logo from './logo.svg'
+import { BaseDotsamaWallet } from '../base-dotsama-wallet' // Importing the base class for Dotsama wallets
+import logo from './logo.svg' // Importing the logo image for Manta Wallet
 
+// Define the MantaWallet class, extending BaseDotsamaWallet
 export class MantaWallet extends BaseDotsamaWallet {
-  extensionName = 'manta-wallet-js'
-  title = 'Manta Wallet'
+  extensionName = 'manta-wallet-js' // The identifier for the Manta Wallet extension
+  title = 'Manta Wallet' // The title that will be displayed to users
   installUrl =
-    'https://chrome.google.com/webstore/detail/manta-wallet/enabgbdfcbaehmbigakijjabdpdnimlg'
+    'https://chrome.google.com/webstore/detail/manta-wallet/enabgbdfcbaehmbigakijjabdpdnimlg' // URL for users to install Manta Wallet
   noExtensionMessage =
-    'You can use any Polkadot compatible wallet but we recommend using Talisman'
+    'You can use any Polkadot compatible wallet but we recommend using Talisman' // Message shown if the extension is not found
   logo = {
-    src: logo,
-    alt: 'Manta Logo',
+    src: logo, // Path to the logo image for Manta Wallet
+    alt: 'Manta Logo', // Alt text for accessibility, describing the logo
   }
 }
