6959096137d6756c3deb12a0df456768b9753a68
fromancient <cantillo.joiner@technoincor.com>
2025-02-20 07:10:15 -0500

    Update types.ts
    Co-Authored-By: netel-here <196799721+netel-here@users.noreply.github.com>
    Co-Authored-By: netel-pleb <12684117+Netel-pleb@users.noreply.github.com>

--- packages/connect-components/src/lib/WalletSelect/types.ts
+++ packages/connect-components/src/lib/WalletSelect/types.ts
@@ -1,11 +1,19 @@
 import { Wallet } from '@polkadot/connect-wallets'
 
+// Interface defining the properties for a list component with clickable items
 export interface ListWithClickProps<T> {
+  // Optional array of items of generic type T
   items?: T[]
+
+  // Optional flag to indicate if items can be installed
   makeInstallable?: boolean
+
+  // Callback function to handle click events on an item of type T
   onClick: (item: T) => unknown
 }
 
+// Interface defining the properties required for a component that uses a wallet
 export interface WithWalletProps {
+  // The wallet object that will be used in the component
   wallet: Wallet
 }
