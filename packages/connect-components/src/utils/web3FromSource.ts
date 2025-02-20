8a6b5d0bcb67f8f477ff2e3c5e50ff919feeb44f
fromancient <cantillo.joiner@technoincor.com>
2025-02-20 07:06:47 -0500

    Update web3FromSource.ts
    Co-Authored-By: netel-here <196799721+netel-here@users.noreply.github.com>
    Co-Authored-By: netel-pleb <12684117+Netel-pleb@users.noreply.github.com>

--- packages/connect-components/src/utils/web3FromSource.ts
+++ packages/connect-components/src/utils/web3FromSource.ts
@@ -1,10 +1,18 @@
 import { getWalletBySource } from '@polkadot/connect-wallets'
 
+// Function to retrieve the wallet extension from the selected wallet source
 export function web3FromSource() {
+  // Retrieve the name of the selected wallet from local storage
   const selectedItem = localStorage.getItem(
     '@polkadot-connect/selected-wallet-name',
   )
+
+  // Get the wallet object using the selected wallet name
   const wallet = getWalletBySource(selectedItem as string)
+
+  // Extract the wallet extension from the wallet object
   const extension = wallet?.extension
+
+  // Return the wallet extension, which can be used for further interactions
   return extension
 }
