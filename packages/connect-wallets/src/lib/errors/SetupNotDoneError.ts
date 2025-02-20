5cbe9d6d446197628aec2cdcc3f98c6bb44737a8
fromancient <cantillo.joiner@technoincor.com>
2025-02-19 19:23:52 -0500

    Update SetupNotDoneError.ts
    Co-Authored-By: netel-here <196799721+netel-here@users.noreply.github.com>

--- packages/connect-wallets/src/lib/errors/SetupNotDoneError.ts
+++ packages/connect-wallets/src/lib/errors/SetupNotDoneError.ts
@@ -1,5 +1,6 @@
-import { BaseWalletError } from './BaseWalletError'
+import { BaseWalletError } from './BaseWalletError' // Importing the base wallet error class
 
+// Define the SetupNotDoneError class, extending BaseWalletError
 export class SetupNotDoneError extends BaseWalletError {
-  readonly name = 'SetupNotDoneError'
+  readonly name = 'SetupNotDoneError' // Setting the name property to identify the specific error type
 }
