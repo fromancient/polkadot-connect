07a158eddc82e50eb11824fc4d8b4f7b72481e35
fromancient <cantillo.joiner@technoincor.com>
2025-02-19 19:18:07 -0500

    Update index.ts
    Co-Authored-By: David Leal <293+david@users.noreply.github.com>
    Co-Authored-By: James Darling <james@abscond.org>
    Co-Authored-By: David <86869373+huscky@users.noreply.github.com>
    Co-Authored-By: netel-here <196799721+netel-here@users.noreply.github.com>

--- packages/connect-wallets/src/lib/aleph-zero-wallet/index.ts
+++ packages/connect-wallets/src/lib/aleph-zero-wallet/index.ts
@@ -1,14 +1,15 @@
-import { BaseDotsamaWallet } from '../base-dotsama-wallet'
-import logo from './logo.svg'
+import { BaseDotsamaWallet } from '../base-dotsama-wallet' // Importing the base wallet class
+import logo from './logo.svg' // Importing the wallet logo
 
+// Define the AlephZeroWallet class, extending BaseDotsamaWallet
 export class AlephZeroWallet extends BaseDotsamaWallet {
-  extensionName = 'aleph-zero'
-  title = 'Aleph Zero Signer'
-  installUrl = 'https://alephzero.org/signer'
+  extensionName = 'aleph-zero' // The name of the wallet extension
+  title = 'Aleph Zero Signer' // The title displayed for the wallet
+  installUrl = 'https://alephzero.org/signer' // URL for users to install the wallet extension
   noExtensionMessage =
-    'You can use any Polkadot compatible wallet but we recommend using Talisman'
+    'You can use any Polkadot compatible wallet but we recommend using Talisman' // Message to display when there is no extension found
   logo = {
-    src: logo,
-    alt: 'Aleph Zero Logo',
+    src: logo, // The source of the logo image
+    alt: 'Aleph Zero Logo', // Alternative text for the logo
   }
 }
