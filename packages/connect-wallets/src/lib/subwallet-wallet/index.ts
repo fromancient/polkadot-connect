389f88e905276dc9f5787dbf390366928dbba827
fromancient <cantillo.joiner@technoincor.com>
2025-02-19 19:30:36 -0500

    Update index.ts
    Co-Authored-By: netel-here <196799721+netel-here@users.noreply.github.com>

--- packages/connect-wallets/src/lib/subwallet-wallet/index.ts
+++ packages/connect-wallets/src/lib/subwallet-wallet/index.ts
@@ -1,15 +1,16 @@
-import { BaseDotsamaWallet } from '../base-dotsama-wallet'
-import logo from './logo.svg'
+import { BaseDotsamaWallet } from '../base-dotsama-wallet' // Importing the base class for Dotsama wallets
+import logo from './logo.svg' // Importing the logo image for SubWallet
 
+// Define the SubWallet class, extending BaseDotsamaWallet
 export class SubWallet extends BaseDotsamaWallet {
-  extensionName = 'subwallet-js'
-  title = 'SubWallet'
+  extensionName = 'subwallet-js' // The identifier for the SubWallet extension
+  title = 'SubWallet' // The title displayed to users
   installUrl =
-    'https://chrome.google.com/webstore/detail/subwallet/onhogfjeacnfoofkfgppdlbmlmnplgbn?hl=en&authuser=0'
+    'https://chrome.google.com/webstore/detail/subwallet/onhogfjeacnfoofkfgppdlbmlmnplgbn?hl=en&authuser=0' // URL for users to install SubWallet
   noExtensionMessage =
-    'You can use any Polkadot compatible wallet but we recommend using Talisman'
+    'You can use any Polkadot compatible wallet but we recommend using Talisman' // Suggestion for alternative wallet if SubWallet is not installed
   logo = {
-    src: logo,
-    alt: 'Subwallet Logo',
+    src: logo, // Path to the logo image for SubWallet
+    alt: 'Subwallet Logo', // Alt text for accessibility, describing the logo
   }
 }
