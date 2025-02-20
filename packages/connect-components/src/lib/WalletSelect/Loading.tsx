4ec1a233282e53f396a7af523f73978de554968d
fromancient <cantillo.joiner@technoincor.com>
2025-02-19 19:07:23 -0500

    Update Loading.tsx
    Co-Authored-By: David Leal <293+david@users.noreply.github.com>
    Co-Authored-By: James Darling <james@abscond.org>
    Co-Authored-By: David <86869373+huscky@users.noreply.github.com>
    Co-Authored-By: netel-here <196799721+netel-here@users.noreply.github.com>

--- packages/connect-components/src/lib/WalletSelect/Loading.tsx
+++ packages/connect-components/src/lib/WalletSelect/Loading.tsx
@@ -1,17 +1,22 @@
-import styles from './Loading.module.css'
+import styles from './Loading.module.css' // Importing the CSS module for styling the loading component
 
+// Define the Loading functional component
 export const Loading = () => {
   return (
     <>
+      {/* Container for the loading indicator */}
       <div
         style={{
-          width: '100%',
-          display: 'inline-flex',
-          justifyContent: 'center',
+          width: '100%', // Set the width to 100% of the parent element
+          display: 'inline-flex', // Use inline-flex to center the loader
+          justifyContent: 'center', // Center the loader horizontally
         }}
       >
+        {/* Loader animation from the imported styles */}
         <div className={styles['lds-dual-ring']} />
       </div>
+
+      {/* Text below the loader stating what the user should do if it takes too long */}
       <div style={{ textAlign: 'center', margin: '2rem 2rem 0' }}>
         If this is taking a while, please refresh the browser.
       </div>
